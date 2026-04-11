2efd9091d79685583eb20a0137eff026680fd2a8
himanshubokare-Guthub <himanshubokare@gmail.com>
2026-04-11 15:11:19 +0530

    day4 about statefile

---
+++ Day4-statefile/main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name="test"
+  }
+}
+
+resource "aws_instance" "test" {
+    ami = "ami-0ea87431b78a82070"
+    instance_type = "t2.medium"
+    tags = {
+      Name="test1"
+    }
+  
+}
