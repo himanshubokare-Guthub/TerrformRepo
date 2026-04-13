7a26321c4115d7859dc9b240d831f815363e118d
himanshubokare-Guthub <himanshubokare@gmail.com>
2026-04-13 15:17:58 +0530

    day6

---
+++ Day6-statelocking/main.tf
@@ -0,0 +1,16 @@
+resource "aws_vpc" "name" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name="vpc_dev"
+  }
+}
+
+resource "aws_instance" "test" {
+    ami = "ami-0ea87431b78a82070"
+    instance_type = "t2.medium"
+    tags = {
+      Name="test1"
+    }
+}
+
+   
